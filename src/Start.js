994e4c6c186101cc9219b4ff5dc13fc1016c89e9
amin.atakisiyev1@gmail.com <amin.atakisiyev1@gmail.com>
2024-03-04 15:57:54 +0400

    Add Wallet Done

--- src/Start.js
+++ src/Start.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { cards } from "./data";
 
 export default function Start() {
@@ -23,10 +23,40 @@ export default function Start() {
   };
 
   const inputStyle = {
-    
+    position: "absolute",
+    marginTop: "60px",
+    marginLeft: "-200px",
+    width: "195px",
+    height: "30px",
+    fontSize: "18px",
   };
 
   const [wallets, setWallets] = useState(cards);
+  const [newCardName, setNewCardName] = useState("");
+  const [newValyute, setNewValyute] = useState("");
+
+  function handleObject() {
+    setWallets([
+      ...wallets,
+      {
+        id: wallets.length + 1,
+        name: newCardName,
+        currency: newValyute,
+        incomes: [],
+        expenses: [],
+      },
+    ]);
+  }
+
+  function cardName(e) {
+    e.preventDefault();
+    setNewCardName(e.target.value);
+  }
+
+  function handleValyute(e) {
+    e.preventDefault();
+    setNewValyute(e.target.value);
+  }
 
   return (
     <div style={mainStyle}>
@@ -34,6 +64,7 @@ export default function Start() {
         <img
           style={{ width: "50%" }}
           src="https://cdn.trend.az/2022/10/18/unibank_logo_181022.jpg"
+          alt="UniBank Logo"
         ></img>
       </div>
 
@@ -43,14 +74,15 @@ export default function Start() {
           height: "40px",
           paddingLeft: "10px",
           fontSize: "18px",
+          marginTop: "10px",
         }}
       >
-        {wallets.map((w) => (
-          <option>{w.name}</option>
+        {wallets.map((w, index) => (
+          <option key={index}>{w.name}</option>
         ))}
       </select>
 
-      <input style={inputStyle}></input>
+      <input style={inputStyle} onChange={cardName}></input>
 
       <select
         style={{
@@ -60,6 +92,7 @@ export default function Start() {
           marginLeft: "50px",
           fontSize: "18px",
         }}
+        onChange={handleValyute}
       >
         <option value="usd">USD</option>
         <option value="eur">EUR</option>
@@ -72,11 +105,12 @@ export default function Start() {
           border: "2px solid #ff6400",
           position: "absolute",
           marginTop: "110px",
-          marginLeft: "-400px",
+          marginLeft: "-420px",
           width: "150px",
           height: "50px",
           fontSize: "18px",
         }}
+        onClick={handleObject}
       >
         Add New Wallet
       </button>
